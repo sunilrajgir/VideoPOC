8385bb828636cbe9d330403ddf86c0eca706a013
Sunil-Kumar1 <Sunil.Kumar1@timesinternet.in>
2020-12-20 10:55:07 +0530

    Empty project

---
+++ TestVideo/TestVideo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestVideo
+//
+//  Created by sunil.kumar1 on 12/20/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
